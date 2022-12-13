62bab0c6105f0c1cc7971901c61a79ae2ca2551b
agathecrsn <119633436+agathecrsn@users.noreply.github.com>
2022-12-13 21:33:28 +0000

    Add files via upload

---
+++ 5_group_assignment/Group_assignment_question1.R
@@ -0,0 +1,79 @@
+##### GROUP ASSIGNMENT QUESTION 1 ######
+
+#datasets withthe ISLR book
+# Import packages ISLR, dplyr, ggplot2
+library(ISLR)
+library(dplyr)
+library(ggplot2)
+
+#######################
+### 1. IMPORT DATA ####
+#######################
+
+## This dataset comes with the ISLR book
+Credit <- read.csv('./data/Credit.csv')
+
+
+### 2. SUBSET OPERATIONS ###
+
+# Total Balance and average Balance
+sum(Credit$Balance)
+mean(Credit$Balance)
+
+### filter ###
+# Student=='Yes' 
+filter(Credit,Student=='Yes')
+# Married=='Yes' AND Age>40
+filter(Credit,Married=='Yes',Age>40)
+
+# dataframe : Student=='Yes' 
+df1 <- Credit[Credit$Student=='Yes',]
+df2 <- Credit[Credit$Married=='Yes' & Credit$Age>40,]
+
+# Total Balance and average Balance when Student=='Yes' 
+sum(df1$Balance)
+# Total Balance and average Balance when Married=='Yes' AND Age>40 
+sum(df2$Balance)
+
+##Histogram##
+# Histogram plot of Balance
+p <- ggplot(data=Credit, aes(x=Balance))
+p + geom_histogram() + labs(x='Balance', y='Number of individuals', title='Histogram plot of Balance')
+
+# Histogram plot of Limit
+p <- ggplot(data=Credit, aes(x=Limit))
+p + geom_histogram() + labs(x='Limit', y='Number of individuals', title='Histogram plot of Limit')
+
+##Boxplot##
+# Boxplot of Balance by Student
+p <- ggplot(data=Credit, aes(x=Student, y=Balance))
+p + geom_boxplot() 
+
+
+###Addition of two numbers entered by the user - Question 1.7 ###
+# Take two numbers as imputs
+a <- readline(prompt='Enter your first number: ')
+b <- readline(prompt='Enter your second number: ')
+# Define the two numbers as numeric
+a <- as.numeric(a)
+b <- as.numeric(b)
+# Calculation of the addition of the two numbers
+c <- a + b
+# Display of the result
+print(paste("Addition of the two numbers is: ",c))
+
+
+### Creation of a data frame - Question 1.8###
+# Creation of a data frame when Income > 30 & Age > 29
+cr <- Credit[Credit$Income>30 & Credit$Age>29,]
+subset(cr)
+
+# Print the number of colums and rows of the data frame & display of the results
+nb.row <- nrow(cr)
+nb.col <- ncol(cr)
+print(paste("The number of rows in the dataset is: ",nb.row))
+print(paste("The number of columns in the dataset is: ",nb.col))
+
+# Write the dataset as a csv file 
+write.csv(cr,"database.csv")
+
---
+++ 5_group_assignment/Group_assignment_question2.R
@@ -0,0 +1,120 @@
+##### GROUP ASSIGNMENT QUESTION 2 ######
+
+#######################
+### 1. IMPORT DATA ####
+#######################
+
+#import data
+# Data: Default dataset in the ISLR has sample data on credit card customers
+# Import package ISLR
+library(ISLR)
+df <- Default
+
+#structure of the dataset
+str(df)
+# summary stats
+summary(df)
+
+#####################
+### 2. CLEAN DATA ###
+#####################
+
+#check for NAs/missing values
+any(is.na(df))
+
+### set correct data types for columns
+## factor() : set categorical columns as factors
+# df$var <- factor(df$factor)
+df$default <- factor(df$default)
+df$student <- factor(df$student)
+
+#######################
+### 3. BUILD MODEL ####
+#######################
+
+## Split data into Train and Test Groups
+# Import package caTools
+library(caTools)
+
+#split up the sample. a boolean vector split is generated
+# SplitRatio = percent of sample==TRUE
+split <- sample.split(df$default, SplitRatio = 0.75) # SplitRatio = percent of sample==TRUE
+
+# Training Data
+train = subset(df, split == TRUE)
+
+# Testing Data
+test = subset(df, split == FALSE)
+
+## TRAIN model on train group
+# logistic regression model in order to predict Default = Yes
+
+# how is dummy variable created for default
+contrasts(df$default)  # Yes=1, No=0 ; model will be trained for probability of default
+
+## Train the model for default = Yes
+glm.fits=glm(default ~ student + balance + income , data=train ,family=binomial)
+
+#model summary
+summary(glm.fits)
+
+# model trained for probability of default=Yes
+contrasts(train$default)  # Yes=1, No=0
+
+# coef() function in order to access just the coefficients for this fitted model
+coef(glm.fits)
+
+########################
+### 5. TEST  MODEL  ####
+########################
+
+## TEST MODEL USING TEST DATA 
+
+# vector with predicted PROBABILITY of default using and test data, using the mdoel created using train data
+glm.probs=predict(glm.fits,newdata=test,type="response")
+# print first 10 probabilities
+glm.probs[1:10]
+
+# convert these predicted probabilities into class labels, Yes or No
+glm.pred <- ifelse(glm.probs > 0.5,"Yes","No")
+
+## CONFUSION MATRIX 
+#in order to determine how many observations were correctly or incorrectly classified.
+table(glm.pred,test$default)
+
+# accuracy (tp+tn)/total
+(22+2407)/(2500)  # 0.97 -  - But high false negatives. model not good.
+
+# recall tp/(tp+fn) - what proportion of the true defaulters were predicted as defaulter
+22/(22+10)  ## 69% recall
+
+# precision tp/(tp+fp) - of the ones predicted Yes (Defaulter), how many are actually Yes (defaulter)
+22/(22+61)  ## 27%
+
+## misclassification = 1 - accuracy = 1 - 0.97 = 3%
+mean(glm.pred!=test$default)
+
+###################################################
+### MODELING ITERATION 2 - sort class imbalance ###
+###################################################
+
+# Import package dplyr
+library(dplyr)
+
+## SORT CLASS IMBALANCE IN PREDICTOR
+# defaulters - 333
+df.yes <- df[df$default=='Yes',]
+
+# non-defaulters - 9667
+df.no <- df[df$default=='No',]
+
+
+
+
+
+
+
+
+
+
+
